df9c116eb163559c9864bec52bb9034bf3d90af5
qzhou14 <qzhou14@cs.binghamton.edu>
2025-09-11 10:07:50 -0400

    make it look slightly better

--- lab2/fahrenheit.c
+++ lab2/fahrenheit.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-	printf("give me a float to convert to celcius");
+	printf("give me a float to convert to celcius:");
 	float f;
 	scanf("%f", &f);
 	float c = ((f - 32.0)/1.8);
-	printf(" %f", c);
+	printf("Celsius: %f", c);
 }
