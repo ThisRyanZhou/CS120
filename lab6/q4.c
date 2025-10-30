792f4565087a3acdec50fb07d9efc5a6faef1efd
1o8ryan <1o8zhouryan@gmail.com>
2025-10-30 10:31:50 -0400

    need to compile and run the file

---
+++ lab6/q4.c
@@ -0,0 +1,16 @@
+#includes <stdio.h>
+
+int main()
+{
+	int b = 798;
+	int a = 0
+	int max = ((a << 5) | (b << 3) | (a & b)) & 0xFE90
+	for(a = 0; a < 99; a++){
+		int newVal = ((a << 5) | (b << 3) | (a & b)) & 0xFE90;
+		if(max > newVal){
+			max = newVal;
+		}
+	}
+	printf("highest val is %d", max);
+}
+		
